72353336ea57439ac977a8e1945367c4286490d3
timothy adigun <adigun.timothy@gmail.com>
2025-11-21 16:42:54 +0000

    learning exception in elixir

--- practiced/Intro/ex07/except/lib/except.ex
+++ practiced/Intro/ex07/except/lib/except.ex
@@ -1,3 +1,7 @@
+defmodule Planet do
+  defstruct name: "", distance: 0
+end
+
 defmodule Except do
   @moduledoc """
   Documentation for `Except`.
@@ -12,40 +16,31 @@ defmodule Except do
       :world
 
   """
-  def fall_velocity(%Planet{name: name, gravity: gravity, distance: distance}) do
+  def fall_velocity(%Planet{name: name, distance: distance}) do
     fall_velocity(name, distance)
   end
 
-  def fall_velocity(:earth, distance) do
-    fall_velocity(:earth, distance, get_gravity(:earth))
-  end
-
-  def fall_velocity(:moon, distance) do
-    fall_velocity(:moon, distance, get_gravity(:moon))
-  end
-
-  def fall_velocity(:mars, distance) do
-    fall_velocity(:mars, distance, get_gravity(:mars))
-  end
-
-  defp fall_velocity(name, distance, gravity) do
+  defp fall_velocity(name, distance) do
     try do
       :math.sqrt(2 * get_gravity(name) * distance)
     rescue
+      # we can specified all the cases of error that could occured
       error -> error
     end
   end
 
-  defp get_gravity(name \\ :earth) do
-    gravity =
-      case name do
-        :earth -> 9.8
-        :moon -> 1.6
-        :mars -> 3.71
-      end
-  end
-end
+  defp get_gravity(name) do
+    case name do
+      :earth ->
+        9.8
 
-defmodule Planet do
-  defstruct name: "" \\ :earth, gravity: 0.0 \\ 9.8, distance: 0
+      :moon ->
+        1.6
+
+      :mars ->
+        3.71
+        # with the catch all value default one would see the error
+        # _ -> 9.8
+    end
+  end
 end
