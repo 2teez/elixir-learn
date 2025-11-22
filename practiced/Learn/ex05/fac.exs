14a8c80073adb99e2eab49da5a98a5110784a777
timothy adigun <adigun.timothy@gmail.com>
2025-11-22 21:13:35 +0000

    adjustment of the type specification in file

--- practiced/Learn/ex05/fac.exs
+++ practiced/Learn/ex05/fac.exs
@@ -12,15 +12,26 @@ defmodule Fac do
 
   @type nums :: pos_integer()
 
-  @spec torial!(@nums()) :: @nums()
+  @spec torial!(nums()) :: nums()
   def torial!(0), do: 1
 
-  @spec torial!(@nums()) :: @nums()
+  @spec torial!(nums()) :: nums()
   def torial!(n), do: torial!(n, 1)
 
-  @spec torial!(@nums(), @nums()) :: @nums()
+  @spec torial!(nums(), nums()) :: nums()
   defp torial!(0, acc), do: acc
 
-  @spec torial!(@nums(), @nums()) :: @nums()
+  @spec torial!(nums(), nums()) :: nums()
   defp torial!(n, acc), do: torial!(n - 1, acc * n)
+
+  @doc """
+  of function takes positive integer denoted by n type.
+  When nums = 0, it returns 1, and for all other numbers
+  if does the factorial of the number
+  """
+  @spec of(nums()) :: nums()
+  def of(0), do: 1
+
+  @spec of(nums()) :: nums()
+  def of(n), do: n * of(n - 1)
 end
