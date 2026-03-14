3865b0ca1c8c432f6e91dc8654d8fb75ff7021ce
timothy adigun <adigun.timothy@gmail.com>
2026-03-14 22:27:16 +0000

    function is sums functions

---
+++ junkz/mm/sums.exs
@@ -0,0 +1,23 @@
+
+    defmodule Sums do
+        @moduledoc """
+          Documentation for Sums
+        """
+
+        @doc """
+
+        # Example
+        """
+
+        def of(0), do: 0
+        def of(n), do: n + of(n-1)
+
+        def len([]), do: 0
+        def len([_|t]), do: 1 + len(t)
+
+        def sum([]), do: 0
+        def sum([h|t]), do: h + sum(t)
+
+        def reduce([], _fun, acc), do: acc
+        def reduce([h|t], fun, acc), do: reduce(t, fun, fun.(h, acc))
+    end
