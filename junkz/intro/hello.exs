06b18c37104832a3cd54f7f26293a94fa4499211
timothy adigun <adigun.timothy@gmail.com>
2026-03-14 13:39:28 +0000

    hello world exs file

---
+++ junkz/intro/hello.exs
@@ -0,0 +1,15 @@
+
+    defmodule Hello do
+        @moduledoc """
+          Documentation for Hello
+        """
+
+        @doc """
+
+        # Example
+        """
+
+        def hello() do
+            IO.puts "Hello, World!"
+        end
+    end
