4c6244d78c7665961dd6cd71ef80934bba5c394a
timothy adigun <adigun.timothy@gmail.com>
2026-03-14 21:55:04 +0000

    times function

---
+++ junkz/mm/times.exs
@@ -0,0 +1,15 @@
+
+    defmodule Times do
+        @moduledoc """
+          Documentation for Times
+        """
+
+        @doc """
+
+        # Example
+        """
+
+        def double(n), do: n * 2
+        def triple(n), do: n * 3
+        def quadruple(n), do: double(n) * double(n)
+    end
