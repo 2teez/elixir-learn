bd601e686ac78771ab20aa1d778bda4f9649ec71
timothy adigun <adigun.timothy@gmail.com>
2026-03-18 04:38:40 +0000

    another file to practice structure in exs

---
+++ practiced/prac_struct/participants.exs
@@ -0,0 +1,16 @@
+
+    defmodule Participants do
+        @moduledoc """
+          Documentation for Participants
+        """
+
+        @doc """
+
+        # Example
+        """
+
+        def hello() do
+            :world
+        end
+    end
+    
