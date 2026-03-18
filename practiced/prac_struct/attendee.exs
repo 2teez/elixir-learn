8612009b8072580cdac1f48a7d501af7c3d53f28
timothy adigun <adigun.timothy@gmail.com>
2026-03-18 04:38:13 +0000

    practice structure in elixir

---
+++ practiced/prac_struct/attendee.exs
@@ -0,0 +1,16 @@
+
+    defmodule Attendee do
+        @moduledoc """
+          Documentation for Attendee
+        """
+
+        @doc """
+
+        # Example
+        """
+
+        def hello() do
+            :world
+        end
+    end
+    
