e0e7311962984e84fc8b646cf58b4f0ea11d8d38
timothy adigun <adigun.timothy@gmail.com>
2026-03-19 08:17:13 +0000

    using receive and send macros

--- sa/async_math.exs
+++ sa/async_math.exs
@@ -5,12 +5,21 @@
         """
 
         @doc """
-
-        # Example
+        start function for processes
         """
 
-        def hello() do
-            :world
+        def start() do
+            receive do
+              {:sum, [x, y], pid} ->
+                send pid, {:result, x + y}
+            end
+            start()
         end
     end
-    
+
+    pid = spawn(Async_math, :start, [])
+    send pid, {:sum, [5, 3], self()}
+    receive do
+      {:result, result} ->
+        IO.puts("Result: #{result}")
+    end
