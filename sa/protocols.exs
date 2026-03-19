c695463a4f5816d8d238150e9bd4e4e1a93fc988
timothy adigun <adigun.timothy@gmail.com>
2026-03-19 01:54:54 +0000

    structure and spec used

--- sa/protocols.exs
+++ sa/protocols.exs
@@ -1,16 +1,35 @@
+defmodule Customer do
+  @moduledoc"""
+  Represents a customer.
+  """
+  @type entity_id() :: String.t()
 
-    defmodule Protocols do
-        @moduledoc """
-          Documentation for Protocols
-        """
+  @type t() :: %Customer{id: entity_id(), first_name: String.t(), last_name: String.t()}
+  defstruct id: "", first_name: "", last_name: ""
+end
 
-        @doc """
+defmodule CustomerDuo do
+  @moduledoc"""
+  Represents a duo of customers.
+  """
 
-        # Example
-        """
+  @doc """
+  Retrieves a customer by their entity ID.
 
-        def hello() do
-            :world
-        end
+  ## Parameters
+
+  - `entity_id`: The unique identifier of the customer.
+
+  ## Returns
+
+  - `{:ok, customer}`: The customer with the given entity ID.
+  - `{:error, :not_found}`: No customer found with the given entity ID.
+  """
+    @spec get_customer(Customer.t(), entity_id()) :: {:ok, Customer.t()} | {:error, :not_found}
+    def get_customer(c, id) do
+      case c do
+        %Customer{id: ^id} = customer -> {:ok, customer}
+        _ -> {:error, :not_found}
+      end
     end
-    
+end
