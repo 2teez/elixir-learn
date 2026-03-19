26f818200d06a9d3eb635b1af157de1a928e2889
timothy adigun <adigun.timothy@gmail.com>
2026-03-19 17:13:30 +0000

    still using behaviour in elixir

--- sa/behaviour.exs
+++ sa/behaviour.exs
@@ -1,12 +1,21 @@
-defmodule Talking do
+defmodule AnimalTalking do
   @callback def say(String.t()) :: :ok
 end
 
 defmodule TalkingDog do
-  @behaviour Talking
+  @behaviour AnimalTalking
 
   @impl true
   def say(message) do
     IO.puts("Dog says: #{woof}")
+  end
+end
+
+defmodule TalkingCat do
+  @behaviour AnimalTalking
+
+  @impl true
+  def say(message) do
+    IO.puts("Cat says: #{miaoo}")
   end
 end
