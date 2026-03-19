16481433c1aa980e0f790ce7e72ca7159ae20968
timothy adigun <adigun.timothy@gmail.com>
2026-03-19 07:38:23 +0000

    using elixir platforms

---
+++ sa/async_math.exs
@@ -0,0 +1,16 @@
+
+    defmodule Async_math do
+        @moduledoc """
+          Documentation for Async_math
+        """
+
+        @doc """
+
+        # Example
+        """
+
+        def hello() do
+            :world
+        end
+    end
+    
