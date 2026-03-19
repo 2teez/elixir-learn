a3441503bcd0e5857f2f3d4bfc16ef9554b060ca
timothy adigun <adigun.timothy@gmail.com>
2026-03-19 02:12:01 +0000

    using protocols in esx

--- sa/protocols.exs
+++ sa/protocols.exs
@@ -13,6 +13,17 @@ defmodule CustomerDuo do
   Represents a duo of customers.
   """
 
+  @type entity_id() :: String.t()
+
+  @doc """
+  Creates a new `Customer` with the given customers.
+  """
+  @spec new(entity_id(), String.t(), String.t()) :: Customer.t()
+  def new(id, first_name, last_name) do
+    %Customer{id: id, first_name: first_name, last_name: last_name}
+  end
+
+
   @doc """
   Retrieves a customer by their entity ID.
 
@@ -25,6 +36,7 @@ defmodule CustomerDuo do
   - `{:ok, customer}`: The customer with the given entity ID.
   - `{:error, :not_found}`: No customer found with the given entity ID.
   """
+
     @spec get_customer(Customer.t(), entity_id()) :: {:ok, Customer.t()} | {:error, :not_found}
     def get_customer(c, id) do
       case c do
@@ -32,4 +44,18 @@ defmodule CustomerDuo do
         _ -> {:error, :not_found}
       end
     end
+end
+
+defprotocol Printable do
+  @doc """
+  Returns a string representation of the given value.
+  """
+  @spec to_csv(data :: any()) :: String.t()
+  def to_csv(data)
+end
+
+defimpl Printable, for: Customer do
+  def to_csv(customer) do
+    [customer.id, customer.first_name, customer.last_name] |> Enum.join(",")
+  end
 end
