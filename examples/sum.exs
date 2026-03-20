a4f907fef5aba92e890c8c005ce4d789be3ceafe
timothy adigun <adigun.timothy@gmail.com>
2026-03-20 22:11:15 +0000

    summation and exercise

--- examples/sum.exs
+++ examples/sum.exs
@@ -20,4 +20,24 @@
         defp tail_sum([], acc), do: acc
         defp tail_sum([h|t], acc), do: tail_sum(t, h + acc)
     end
-    
+
+    defmodule Transformation do
+      @moduledoc """
+        Documentation for Transformation
+      """
+
+      @doc """
+        Transforms a list by flattening, reversing, and squaring each element.
+
+        # Example
+        [1,[[2], 3]] |> Transformation.transform() #=> [9, 4, 1]
+        """
+
+      @spec transform(list()) :: list()
+
+      def transform(lst) when is_list(lst) do
+        lst |> List.flatten()
+            |> Enum.reverse()
+            |> Enum.map(&(&1 * &1))
+      end
+    end
