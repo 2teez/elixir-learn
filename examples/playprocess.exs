52441670afad4bedd2268ca24b2778e939affd61
timothy adigun <adigun.timothy@gmail.com>
2026-03-23 13:21:45 +0000

    practice process in elixir

---
+++ examples/playprocess.exs
@@ -0,0 +1,16 @@
+
+    defmodule Playprocess do
+        @moduledoc """
+          Documentation for Playprocess
+        """
+
+        @doc """
+
+        # Example
+        """
+
+        def hello() do
+            :world
+        end
+    end
+    
