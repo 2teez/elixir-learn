747c857c33784e4470305806b5a39f2201275a91
timothy adigun <adigun.timothy@gmail.com>
2026-03-23 20:41:04 +0000

    process practice in exs

--- examples/playprocess.exs
+++ examples/playprocess.exs
@@ -9,8 +9,31 @@
         # Example
         """
 
+        def report do
+          receive do
+            msg -> IO.puts("Received: #{msg}")
+          end
+          report()
+        end
+
+        def hello_world() do
+
+          for greet <- ["hi", "howdy", "hello", "henry"] |> Enum.shuffle() do
+              pid = spawn(Playprocess, :hello, [])
+              send(pid, {self(), {:ok, greet, "world"}})
+            end
+
+          receive do
+            {:ok, msg}  -> IO.puts("Received: #{msg}")
+          end
+          #hello_world()  # for infinite loop
+        end
+
         def hello() do
-            :world
+            receive do
+              {pid, {:ok, greet, msg}} -> send(pid, {:ok, "#{greet} #{msg}"})
+              _ -> IO.puts("don't know how to process this message")
+            end
+            hello()
         end
     end
-    
