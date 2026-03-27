5f20ec8ac7e69a93e7faae0ef15bffd9d5f9e65a
timothy adigun <adigun.timothy@gmail.com>
2026-03-27 21:01:06 +0000

    learning spawn in elixir

---
+++ examples/spawn/spawn1.exs
@@ -0,0 +1,26 @@
+defmodule Spawn1 do
+  @moduledoc """
+    Documentation for Spawn1
+  """
+
+  @doc """
+  # Example
+  """
+
+  def client() do
+    pid = spawn(Spawn1, :greet, [])
+    send(pid, {self(), "world"})
+
+    receive do
+      {:ok, msg} ->
+        IO.puts(msg)
+    end
+  end
+
+  def greet() do
+    receive do
+      {pid, msg} ->
+        send(pid, {:ok, "Hello, #{msg}"})
+    end
+  end
+end
