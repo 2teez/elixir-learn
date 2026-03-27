fe74d5f9d21256ba28ad98e7ef5bfaac555ba278
timothy adigun <adigun.timothy@gmail.com>
2026-03-27 20:18:22 +0000

    formatting the elixir page

--- examples/ping_pong.exs
+++ examples/ping_pong.exs
@@ -1,29 +1,29 @@
+defmodule PingPong do
+  @moduledoc """
+    Documentation for Ping-pong
+  """
 
-    defmodule PingPong do
-        @moduledoc """
-          Documentation for Ping-pong
-        """
+  @doc """
 
-        @doc """
+  # Example
+  """
 
-        # Example
-        """
+  def pong() do
+    pid = spawn(PingPong, :ping, [])
+    send(pid, {self(), :ping})
 
-        def pong() do
-          pid = spawn(PingPong, :ping, [])
-          send(pid, {self(), :ping})
-
-          receive do
-            {:pong, _} -> "pong"
-            {:exit, reason} -> exit(reason)
-          end
-        end
-
-        def ping() do
-            receive do
-                {pid, :ping} -> send(pid, {:pong, IO.puts("ping")})
-                {:exit, reason} -> exit(reason)
-            end
-            ping()
-        end
+    receive do
+      {:pong, _} -> "pong"
+      {:exit, reason} -> exit(reason)
     end
+  end
+
+  def ping() do
+    receive do
+      {pid, :ping} -> send(pid, {:pong, IO.puts("ping")})
+      {:exit, reason} -> exit(reason)
+    end
+
+    ping()
+  end
+end
