92e8e988f60c910d0b93a1d7e85cb6adb75d4cd7
timothy adigun <adigun.timothy@gmail.com>
2026-03-28 19:19:30 +0000

    formatted the sum file

--- examples/sum.exs
+++ examples/sum.exs
@@ -1,43 +1,43 @@
+defmodule Summation do
+  @moduledoc """
+    Documentation for Sum
+  """
 
-    defmodule Summation do
-        @moduledoc """
-          Documentation for Sum
-        """
+  @doc """
 
-        @doc """
+  # Example
+  """
 
-        # Example
-        """
+  @spec sum(list()) :: integer
 
-        @spec sum(list()) :: integer
+  def sum([]), do: 0
+  def sum([h | t]), do: h + sum(t)
 
-        def sum([]), do: 0
-        def sum([h | t ]), do: h + sum(t)
+  @spec tail_sum(list()) :: integer
 
-        @spec tail_sum(list()) :: integer
+  def tail_sum(lst), do: tail_sum(lst, 0)
+  defp tail_sum([], acc), do: acc
+  defp tail_sum([h | t], acc), do: tail_sum(t, h + acc)
+end
 
-        def tail_sum(lst), do: tail_sum(lst, 0)
-        defp tail_sum([], acc), do: acc
-        defp tail_sum([h|t], acc), do: tail_sum(t, h + acc)
-    end
+defmodule Transformation do
+  @moduledoc """
+    Documentation for Transformation
+  """
 
-    defmodule Transformation do
-      @moduledoc """
-        Documentation for Transformation
-      """
+  @doc """
+  Transforms a list by flattening, reversing, and squaring each element.
 
-      @doc """
-        Transforms a list by flattening, reversing, and squaring each element.
+  # Example
+  [1,[[2], 3]] |> Transformation.transform() #=> [9, 4, 1]
+  """
 
-        # Example
-        [1,[[2], 3]] |> Transformation.transform() #=> [9, 4, 1]
-        """
+  @spec transform(list()) :: list()
 
-      @spec transform(list()) :: list()
-
-      def transform(lst) when is_list(lst) do
-        lst |> List.flatten()
-            |> Enum.reverse()
-            |> Enum.map(&(&1 * &1))
-      end
-    end
+  def transform(lst) when is_list(lst) do
+    lst
+    |> List.flatten()
+    |> Enum.reverse()
+    |> Enum.map(&(&1 * &1))
+  end
+end
