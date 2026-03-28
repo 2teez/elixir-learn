db6704acb2e26064cb00a1b913f63b1a701707c6
timothy adigun <adigun.timothy@gmail.com>
2026-03-28 18:53:54 +0000

    implement recurssion

--- examples/mylist.exs
+++ examples/mylist.exs
@@ -8,7 +8,15 @@ defmodule Mylist do
   # Example
   """
 
-  def hello() do
-    :world
-  end
+  def maximum([]), do: nil
+  def maximum([h | t]), do: maximum(t, h)
+  defp maximum([], maxi), do: maxi
+  defp maximum([h | t], maxi) when h > maxi, do: maximum(t, h)
+  defp maximum([h | t], maxi) when h <= maxi, do: maximum(t, maxi)
+
+  def minimum([]), do: nil
+  def minimum([h | t]), do: minimum(t, h)
+  defp minimum([], mini), do: mini
+  defp minimum([h | t], mini) when h < mini, do: minimum(t, h)
+  defp minimum([h | t], mini) when h >= mini, do: minimum(t, mini)
 end
