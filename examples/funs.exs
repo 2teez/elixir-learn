731c309b29979703cf88adcbdc106120ed45f689
timothy adigun <adigun.timothy@gmail.com>
2026-04-01 07:39:02 +0100

    using implmented fetch on elixir

--- examples/funs.exs
+++ examples/funs.exs
@@ -4,11 +4,21 @@ defmodule Funs do
   """
 
   @doc """
+  Fetches the element at the given index from the list.
 
+  Returns `{:ok, element}` if the index is valid, or `:error` if the index is out of bounds.
   # Example
+  iex> Funs.fetch([1, 2, 3], 0)
+  {:ok, 1}
+  iex> Funs.fetch([1, 2, 3], 3)
+  :error
   """
 
-  def hello() do
-    :world
+  @spec fetch(list(), integer()) :: {:ok, integer()} | :error
+  def fetch(list, index), do: fetch(list, index, 0)
+  defp fetch([], _index, _count), do: :error
+
+  defp fetch([head | tail], index, count) do
+    if index == count, do: {:ok, head}, else: fetch(tail, index, count + 1)
   end
 end
