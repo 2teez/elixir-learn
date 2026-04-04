c8b7ed33cbfee5a9c907cc42e2495de569319415
timothy adigun <adigun.timothy@gmail.com>
2026-04-04 05:01:10 +0100

    moved macro folder to junkz folder

---
+++ macros/junkz/macro/math.exs
@@ -0,0 +1,30 @@
+defmodule Math do
+  @moduledoc """
+    Documentation for Math
+  """
+
+  @doc """
+
+  # Example
+  """
+
+  defmacro say({:+, _, [lhs, rhs]}) do
+    quote do
+      lhs = unquote(lhs)
+      rhs = unquote(rhs)
+      result = lhs + rhs
+      IO.puts("#{lhs} plus #{rhs} is #{result}")
+      result
+    end
+  end
+
+  defmacro say({:*, _, [lhs, rhs]}) do
+    quote do
+      lhs = unquote(lhs)
+      rhs = unquote(rhs)
+      result = lhs * rhs
+      IO.puts("#{lhs} times #{rhs} is #{result}")
+      result
+    end
+  end
+end
